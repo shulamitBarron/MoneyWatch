3967a3bcd25c51de7a267fc88f5d6b6486013a20
shulamitBarron <shulamitbarron@gmail.com>
2018-02-19 00:00:38 +0200

    Update generateFacts.js

--- controllers/generateFacts.js
+++ controllers/generateFacts.js
@@ -233,79 +233,77 @@ exports.generateFacts = function (storyId, transactions, accounts) {
                 throw "error";
             }
             break;
-        case "7221df03-f2e3-421e-8667-eea0c6b7c7a5":
-            transactions = transactions.map(function (t) { return (__assign({}, t, { categoryDescription: t.categoryDescription.en })); });
-            theTransactions = common_1.groupBy(transactions.filter(function (t) { return t.Mode === "Out" && t.categoryDescription && t.categoryDescription !== "null"; }), function (item) { return [item.categoryDescription]; });
-            theTransactions = theTransactions.map(function (t) { return common_1.groupBy(t, function (item) {
-                return [(new Date(item.date)).getMonth(), (new Date(item.date)).getFullYear()];
-            }); });
-            theTransactions = theTransactions.map(function (theT) { return theT.filter(function (theTransactions_0) {
-                var curDate = new Date();
-                var t = theTransactions_0[0];
-                var usrYear, usrMonth = new Date(t.date).getMonth() + 1;
-                var curYear, curMonth = curDate.getMonth() + 1;
+               case "7221df03-f2e3-421e-8667-eea0c6b7c7a5":
+            transactions = transactions.map(t => ({...t , categoryDescription: t.categoryDescription.en}));
+            theTransactions = groupBy(transactions.filter(t => t.Mode === "Out" && t.categoryDescription && t.categoryDescription !== "null") , item => [item.categoryDescription]);
+            theTransactions = theTransactions.map(t => groupBy(t , item =>
+                [(new Date(item.date)).getMonth() , (new Date(item.date)).getFullYear()]));
+            theTransactions = theTransactions.map(theT => theT.filter(theTransactions_0 => {
+                const curDate = new Date();
+                let t = theTransactions_0[0];
+                let usrYear , usrMonth = new Date(t.date).getMonth() + 1;
+                let curYear , curMonth = curDate.getMonth() + 1;
                 if ((usrYear = new Date(t.date).getFullYear()) < (curYear = curDate.getFullYear())) {
                     curMonth += (curYear - usrYear) * 12;
                 }
-                var diffMonths = curMonth - usrMonth;
-                return diffMonths && diffMonths < 4;
-            }); });
-            var peri_1 = [2, 3, 4];
-            theTransactions = theTransactions.map(function (theTransaction) { return ({
-                theTransactions: theTransaction,
-                categoryDescription: theTransaction[0][0].categoryDescription,
-                avg: peri_1.map(function (p) {
-                    var trans = theTransaction.filter(function (theTransactions_0) {
-                        var curDate = new Date();
-                        var t = theTransactions_0[0];
-                        var usrYear, usrMonth = new Date(t.date).getMonth() + 1;
-                        var curYear, curMonth = curDate.getMonth() + 1;
+                let diffMonths = curMonth - usrMonth;
+                return diffMonths && diffMonths < 4
+
+            }));
+            const peri = [2 , 3 , 4];
+            theTransactions = theTransactions.map(theTransaction => theTransaction.length && theTransaction[0][0]? ({
+                theTransactions: theTransaction ,
+                categoryDescription: theTransaction[0][0].categoryDescription ,
+                avg: peri.map(p => {
+                    var trans = theTransaction.filter(theTransactions_0 => {
+                        const curDate = new Date();
+                        let t = theTransactions_0[0];
+                        let usrYear , usrMonth = new Date(t.date).getMonth() + 1;
+                        let curYear , curMonth = curDate.getMonth() + 1;
                         if ((usrYear = new Date(t.date).getFullYear()) < (curYear = curDate.getFullYear())) {
                             curMonth += (curYear - usrYear) * 12;
                         }
-                        var diffMonths = curMonth - usrMonth;
+                        let diffMonths = curMonth - usrMonth;
                         return diffMonths === p;
+
                     });
-                    return trans.length ? trans[0].map(function (t) { return +t.amount; }).reduce(function (a, b) { return a + b; }) : 0;
-                }).reduce(function (a, b) { return a + b; }) / peri_1.length
-            }); });
-            theTransactions = theTransactions.map(function (tt) {
-                var lastTr = tt.theTransactions.filter(function (theTransactions_0) {
-                    var curDate = new Date();
-                    var t = theTransactions_0[0];
-                    var usrYear, usrMonth = new Date(t.date).getMonth() + 1;
-                    var curYear, curMonth = curDate.getMonth() + 1;
+                    return trans.length ? trans[0].map(t => +t.amount).reduce((a , b) => a + b) : 0;
+                }).reduce((a , b) => a + b) / peri.length
+            }): theTransaction);
+            theTransactions = theTransactions.map(tt => {
+                const lastTr = tt.theTransactions? tt.theTransactions.filter(theTransactions_0 => {
+                    const curDate = new Date();
+                    let t = theTransactions_0[0];
+                    let usrYear , usrMonth = new Date(t.date).getMonth() + 1;
+                    let curYear , curMonth = curDate.getMonth() + 1;
                     if ((usrYear = new Date(t.date).getFullYear()) < (curYear = curDate.getFullYear())) {
                         curMonth += (curYear - usrYear) * 12;
                     }
-                    var diffMonths = curMonth - usrMonth;
+                    let diffMonths = curMonth - usrMonth;
                     return diffMonths === 1;
-                });
+
+                }): [];
                 return {
-                    sum: lastTr.length ? lastTr[0].map(function (a) { return +a.amount; }).reduce(function (a, b) { return a + b; }) : 0,
-                    categoryDescription: tt.categoryDescription,
-                    Difference: lastTr.length ? lastTr[0].map(function (a) { return +a.amount; }).reduce(function (a, b) { return a + b; }) - tt.avg : 0,
+                    sum: lastTr.length ? lastTr[0].map(a => +a.amount).reduce((a , b) => a + b) : 0 ,
+                    categoryDescription: tt.categoryDescription ,
+                    Difference: lastTr.length ? lastTr[0].map(a => +a.amount).reduce((a , b) => a + b) - tt.avg : 0 ,
                     avg: tt.avg
-                };
+                }
             });
-            theTransactions = theTransactions.sort(function (a, b) { return a.Difference - b.Difference; });
-            var selectedCat_1;
+            theTransactions = theTransactions.sort((a , b) => a.Difference - b.Difference);
+            let selectedCat;
             if (theTransactions.length) {
-                selectedCat_1 = theTransactions[0].categoryDescription;
-            }
-            else {
+                selectedCat = theTransactions[0].categoryDescription;
+            } else {
                 throw "error";
             }
-            theTransactions = transactions.filter(function (t) { return t.mode === "Out" && t.categoryDescription === selectedCat_1; });
-            theAccounts = accounts.filter(function (account) {
-                return theTransactions.filter(function (t) { return t.accountNumber === account.number; }).length > 0;
-            });
+            theTransactions = transactions.filter(t => t.mode === "Out" && t.categoryDescription === selectedCat);
+            theAccounts = accounts.filter(account =>
+                theTransactions.filter(t => t.accountNumber === account.number).length > 0);
             if (theTransactions.length && theAccounts.length) {
-                insightsConfiguration_1.tablesForStory[insightsConfiguration_1.storyIdForInsightId[storyId]].map(function (table) {
-                    return facts[table] = generateTable_1.default(table, theTransactions, theAccounts, [["In"], ["Out"]], periods, selectedCat_1);
-                });
-            }
-            else {
+                tablesForStory[storyIdForInsightId[storyId]].map(table =>
+                    facts[table] = generateTble(table , theTransactions , theAccounts , [["In"] , ["Out"]] , periods , selectedCat));
+            } else {
                 throw "error";
             }
             break;
